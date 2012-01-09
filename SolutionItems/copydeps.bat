78e87f27eac8f3a096ec55fb6d3bdd3072de5865
Nate Hollingsworth <nate@nexia.ca>
2012-01-09 09:04:17 -0500

    Fixing Log4net version

--- SolutionItems/copydeps.bat
+++ SolutionItems/copydeps.bat
@@ -4,4 +4,4 @@
 
 c:\windows\system32\xcopy %COPY_OPTS% "%PKG_DIR%\Common.Logging.2.0.0\lib\2.0\*" .
 c:\windows\system32\xcopy %COPY_OPTS% "%PKG_DIR%\Common.Logging.Log4Net.2.0.0\lib\2.0\*" .
-c:\windows\system32\xcopy %COPY_OPTS% "%PKG_DIR%\log4net.1.2.11\lib\net40-full\*" .
+c:\windows\system32\xcopy %COPY_OPTS% "%PKG_DIR%\log4net.1.2.10\lib\2.0\*" .
